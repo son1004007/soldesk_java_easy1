5ac49ca38ffb630ba01f6b71699b76d70359700b
son007 <75886169+sonkiseok@users.noreply.github.com>
2021-01-19 16:19:57 +0900

    22

--- src/Y0118_ass1.java
+++ src/Y0118_ass1.java
@@ -3,23 +3,23 @@ public class Y0118_ass1 {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		//���� assignmnet. page 397 ���� 27 �������� ���Ͽ� ��� 
+		//占쏙옙占쏙옙 assignmnet. page 397 占쏙옙占쏙옙 27 占쏙옙占쏙옙占쏙옙占쏙옙 占쏙옙占싹울옙 占쏙옙占� 
 		
 		try {
-			PrintWriter pw = new PrintWriter("gugu.txt");
+			PrintWriter pw = new PrintWriter("gugu.txt"); // 주인공이면서 장식
 			String str="";
 			for(int i =1 ; i<=9 ; i++) {
 				for(int j = 2 ; j<=9 ; j++) {
-					System.out.print(j+"*"+i+"="+(j*i)+"\t");// ������
-//					str += j+"*"+i+"="+(j*i)+"\t";
+//					System.out.print(j+"*"+i+"="+(j*i)+"\t");// 占쏙옙占쏙옙占쏙옙
+					str += j+"*"+i+"="+(j*i)+"\t";
 					
 				}
-				System.out.println();//������
-//				str += "\n";
+//				System.out.println();//占쏙옙占쏙옙占쏙옙
+				str += "\n";
 			}
 			
-//			pw.println(str);
-//			pw.close();
+			pw.println(str);
+			pw.close();
 
 		}catch(Exception e) {}
 	
